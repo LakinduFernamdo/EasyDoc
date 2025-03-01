0278b4c5672e0fb22a8768cdc73188bbf3106046
LakinduFernamdo <122218443+LakinduFernamdo@users.noreply.github.com>
2025-03-01 14:47:22 +0530

    Update DocController.js

--- BackEnd/Controllers/DocController.js
+++ BackEnd/Controllers/DocController.js
@@ -1,10 +1,1 @@
-import pool from "../Config/DocDB.js";
-export const getDoctors = async (req, res) => {
-  try {
-    const { rows } = await pool.query("SELECT * FROM doctors");
-    res.json(rows);
-  } catch (err) {
-    console.error("Error fetching doctors:", err.message);
-    res.status(500).send("Server error");
-  }
-};
+
